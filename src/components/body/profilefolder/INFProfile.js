6128813406b64837ed4671c01854f986d8d99857
yongho <yongho6800@gmail.com>
2022-09-21 23:30:06 +0900

    mobile responsive ui prf

--- src/components/body/profilefolder/INFProfile.js
+++ src/components/body/profilefolder/INFProfile.js
@@ -13,19 +13,19 @@ import { useNavigate } from "react-router";
 import TextField from "@mui/material/TextField";
 import FormControl from "@mui/material/FormControl";
 import Avatar from "@mui/material/Avatar";
-import { Form, Alert, Button } from "react-bootstrap";
+
 import { InputLabel, Typography } from "@mui/material";
 import { Input } from "@mui/material";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
 
-
-import Inputpassword from './INFDeleteUser';
+import Inputpassword from "./INFDeleteUser";
 import UploadProfile from "./UploadProfile";
 
 import { async } from "@firebase/util";
@@ -35,10 +35,15 @@ import { useDispatch, useSelector } from "react-redux";
 import { bindActionCreators } from "redux";
 import { actionCreators } from "../../../state/index";
 
+import "./INFProfile.css";
+
+import { BsPersonFill } from "react-icons/bs"
+
+
 const INFProfile = () => {
   const [open, setOpen] = React.useState(false);
   const [scroll, setScroll] = React.useState("paper");
-  const [password, setPassword] = useState('')
+  const [password, setPassword] = useState("");
 
   const dispatch = useDispatch();
   const state = useSelector((state) => state);
@@ -57,7 +62,7 @@ const INFProfile = () => {
   const handleClose = () => {
     setOpen(false);
   };
-  console.log(state.influencer.state.infloginData.role)
+  console.log(state.influencer.state.infloginData.role);
 
   const descriptionElementRef = React.useRef(null);
   useEffect(() => {
@@ -92,129 +97,135 @@ const INFProfile = () => {
   };
 
   const goDelete = () => {
-    navigate("/INFDeleteUser")
-  }
-
+    navigate("/INFDeleteUser");
+  };
 
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        backgroundColor: "#667",
-      }}
-    >
-      <div style={{ width: "50vw" }}>
-        <Box
+    <div className="container">
+      <div style={{ width: "80vw" }}>
+        {/* 프로필카드 컨테이너 */}
+        <div
           style={{
             backgroundColor: "#fff",
           }}
-          sx={{ flexGrow: 1 }}
         >
-          <Grid style={{}}>
-            <div style={{ backgroundColor: "red", margin: "15px" }}>
-              <h1>Hello! It's profile page.</h1>
-              <div
-                style={{
-                  display: "flex",
-                  flexDirection: "row",
-                  alignItems: "flex-end",
-                  backgroundColor: "blue",
-                }}
-              >
-                <Avatar
-                  alt="Remy Sharp"
-                  src={state.influencer.state.infloginData.avatar}
-                  sx={{ width: 170, height: 170 }}
-                />
+          {/* 사진부터 롤까지 상단 컨테이너 */}
+          <div className="img-name-container">
+            {/* 상단프로필 상단박스 */}
+            <div className="top-blank-box">
+            <Avatar
+                alt="Remy Sharp"
+                src={state.influencer.state.infloginData.avatar}
+                sx={{ width: 179, height: 179 }}
+                style={{marginTop: '10vh', marginLeft: '1vh', position: 'absolute'}}
 
-                <div
-                  style={{
-                    marginLeft: "15px",
-                    width: "100%",
-                    height: "170",
-                    backgroundColor: "#fff",
-                  }}
+              />
+            </div>
+            
+            
+
+            {/* 수정버튼 / 정보 컨테이너*/}
+            <div className="btm-box-info-container">
+              {/* 수정버튼 및 클릭시 다이얼로그 */}
+              <div className="bottom-box-container">
+              <div className="bottom-box">
+                <Button variant="outlined" onClick={handleClickOpen("paper")}>
+                  Edit Profile
+                </Button>
+
+                <Dialog
+                  open={open}
+                  onClose={handleClose}
+                  scroll={scroll}
+                  aria-labelledby="scroll-dialog-title"
+                  aria-describedby="scroll-dialog-description"
+                  fullWidth="true"
+                  maxWidth="40px"
+                  style={{ zIndex: 50 }}
                 >
-                  <div style={{ display: "flex", justifyContent: "flex-end" }}>
-                    <div>
-                      <Button onClick={handleClickOpen("paper")}>수정</Button>
+                  <DialogTitle id="scroll-dialog-title">Subscribe</DialogTitle>
+                  <DialogContent dividers={scroll === "paper"}>
+                    <UploadProfile />
+                  </DialogContent>
+                  <DialogActions>
+                    <Button onClick={handleClose}>Cancel</Button>
 
-                      <Dialog
-                        open={open}
-                        onClose={handleClose}
-                        scroll={scroll}
-                        aria-labelledby="scroll-dialog-title"
-                        aria-describedby="scroll-dialog-description"
-                        fullWidth="true"
-                        maxWidth="40px"
-                        style={{zIndex:50}}
-                      >
-                        <DialogTitle id="scroll-dialog-title">
-                          Subscribe
-                        </DialogTitle>
-                        <DialogContent dividers={scroll === "paper"}>
-                          <UploadProfile />
-                        </DialogContent>
-                        <DialogActions>
-                          <Button onClick={handleClose}>Cancel</Button>
+                    <Button
+                      positive
+                      form="my-form"
+                      content="Submit"
+                      value="Submit"
+                      type="Submit"
+                    >
+                      저장
+                    </Button>
+                  </DialogActions>
+                </Dialog>
+              </div>
+              </div>
+              
 
-                          <Button
-                            positive
-                            form="my-form"
-                            content="Submit"
-                            value="Submit"
-                            type="Submit"
-                          >
-                            저장
-                          </Button>
-                        </DialogActions>
-                      </Dialog>
-                    </div>
-                  </div>
-                  <Typography style={{ fontSize: "40px" }}>
-                    {state.influencer.state.infloginData.nickname}
-                  </Typography>
-                  <Typography style={{ fontSize: "25px", color: "grey" }}>
-                    {state.influencer.state.infloginData.role}
-                  </Typography>
+              {/* 중요정보 컨테이너 */}
+              <div className="name-role-container">
+                {/* 나의 닉네임 */}
+                <div style={{ fontSize: "40px" }}>
+                  {state.influencer.state.infloginData.nickname}
+                </div>
+
+                {/* 나의 롤 */}
+                <div style={{ fontSize: "25px", color: "grey" }}>
+                  {state.influencer.state.infloginData.role}
                 </div>
               </div>
-
-              <div>
-                <br />
-                성별
-                {state.influencer.state.infloginData.sex}
-                <br />
-                생일
-                {state.influencer.state.infloginData.birthday}
-                <br />
-                인스타
-                {state.influencer.state.infloginData.insta}
-                <br />
-                이메일
-                {state.influencer.state.infloginData.email}
-                <br />
-                조인체널
-                {state.influencer.state.infloginData.joined_channel}
-                <br />
-                번호
-                {state.influencer.state.infloginData.mobile}
-                <br />
-                태그
-                {state.influencer.state.infloginData.tags}
-                <br />
-                위치
-                {state.influencer.state.infloginData.location}
-                <br />
-              </div>
-              <div>
-              </div>
             </div>
-          </Grid>
+          </div>
 
-          
-        </Box>
+          <div>
+            <br />
+            {/* 정보 컨테이너 */}
+            <div className="specified-info-container">
+              <div className="info-icon">
+                <BsPersonFill />
+              </div>
+              <div className="info-text-container">
+                <div className="info-text-me">
+                  {state.influencer.state.infloginData.sex}
+                </div>
+                <div className="info-text-category">
+                  sex
+                </div>
+
+              </div>
+
+            </div>
+
+            성별
+            {state.influencer.state.infloginData.sex}
+            <br />
+            생일
+            {state.influencer.state.infloginData.birthday}
+            <br />
+            인스타
+            {state.influencer.state.infloginData.insta}
+            <br />
+            이메일
+            {state.influencer.state.infloginData.email}
+            <br />
+            조인체널
+            {state.influencer.state.infloginData.joined_channel}
+            <br />
+            번호
+            {state.influencer.state.infloginData.mobile}
+            <br />
+            태그
+            {state.influencer.state.infloginData.tags}
+            <br />
+            위치
+            {state.influencer.state.infloginData.location}
+            <br />
+          </div>
+          <div></div>
+        </div>
 
         <div style={{ backgroundColor: "#a78" }}>
           <Button onClick={() => goDelete()}>회원탈퇴</Button>
