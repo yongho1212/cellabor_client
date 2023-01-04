2e6fd2f0ed79bb1a407a33b196a7e38abb661517
yongho <yongho6800@gmail.com>
2023-01-05 02:28:27 +0900

    scroll motion

--- src/components/body/homefolder/Home.js
+++ src/components/body/homefolder/Home.js
@@ -4,10 +4,13 @@ import './home.css'
 import Section from '../../Section';
 import { homeObjOne, homeObjTwo, homeObjThree, homeObjFour } from './Data';
 
+import Box from '../../../useScrollFadeIn'
 
 const Home = () => {
   const { user } = useSelector((state) => ({ ...state.user }));
   const dispatch = useDispatch();
+
+  const observer = new IntersectionObserver(() => {})
 
   // const sectionStyle = {
   //   width: "100%",
@@ -18,11 +21,13 @@ const Home = () => {
     <>
       <div className="home_page">
         {/* <div style={{ height: '100vh', backgroundImage: `url(${background})`, backgroundSize: "cover" }} >
-          
         </div> */}
-        <Section {...homeObjOne} />
+        {/* <Section {...homeObjOne} />
         <Section {...homeObjThree} />
-        <Section {...homeObjTwo} />
+        <Section {...homeObjTwo} /> */}
+        <Box num={1} />
+        <Box num={2} />
+        <Box num={3} />
       </div>
     </>
   );
